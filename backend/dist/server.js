f024e6d560cbd98cea7b7b4d04d1851f299fdf2d
Gabriel Lazari Trevisani <gabriel@MacBook-Pro-de-Gabriel.local>
2026-09-11 22:53:07 -0300

    feat(frontend): frontend architecture

---
+++ backend/dist/server.js
@@ -0,0 +1,6 @@
+import { app } from "./app.js";
+import "dotenv/config";
+const port = Number(process.env.PORT ?? 3333);
+app.listen(port, () => {
+    console.log(`HTTP server running on ${port}`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <h1>Frontend ready</h1>;
+}
