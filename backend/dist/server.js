4adf023270eccce34c6c7dd10baf47734ef82ed7
Gabriel Lazari Trevisani <gabriel@MacBook-Pro-de-Gabriel.local>
2026-09-11 22:55:26 -0300

    fix: stop tracking backend build artifacts

--- backend/dist/server.js
+++
@@ -1,6 +0,0 @@
-import { app } from "./app.js";
-import "dotenv/config";
-const port = Number(process.env.PORT ?? 3333);
-app.listen(port, () => {
-    console.log(`HTTP server running on ${port}`);
-});
